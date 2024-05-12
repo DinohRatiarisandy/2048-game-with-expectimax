496234aaf5196c985713976ce17d904a8c39b537
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-05-12 16:42:52 +0300

    gameplay's script

---
+++ Scripts/Gameplay.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+signal bot_solver_playing
+
+var auto_play := false
+
+const DIRECTIONS := ["move_up", "move_right", "move_down", "move_left"]
+
+@onready var board: Board = $Board
+@onready var auto_play_btn: Button = $Buttons/HBoxContainer/AutoPlay
+
+
+func toggle_auto_play_btn() -> void:
+	auto_play = not auto_play
+	if  auto_play_btn.text == "autoplay":
+		auto_play_btn.text = "stop"
+	else:
+		auto_play_btn.text = "autoplay"
+
+
+func _on_keyboard_control_move(direction: String) -> void:
+	board.play(direction)
+
+
+func _on_restart_button_down() -> void:
+	board.restart()
+	auto_play = false
+	auto_play_btn.text = "autoplay"
+	auto_play_btn.disabled = false
+
+
+func _on_auto_play_button_down() -> void:
+	toggle_auto_play_btn()
+	var ai := BotSolver.new()
+	while auto_play:
+		board.play(ai.best_move(board, 3))
+		await get_tree().create_timer(0.25).timeout
+	ai.queue_free()
